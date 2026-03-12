4901176e6fc1e44325441e269d9a55ffde756729
Pahuljot Matharoo <matharoopahuljot@gmail.com>
2026-03-12 02:41:52 +0000

    some code cleanup

--- server.c
+++ server.c
@@ -15,6 +15,8 @@
 #define MSG_EXIT 3
 #define username_length 50
 
+//need to add a lock per socket, so we don't write to same socket at the same time
+
 pthread_mutex_t mutex;
 pthread_mutex_t user_fileMutex;
 pthread_mutex_t group_fileMutex;
@@ -33,6 +35,17 @@ void cleanup() {
     close(sock);
 }
 
+thread_arg* setupThreadArg(user* new_user) {
+    thread_arg *arg = malloc(sizeof(thread_arg));
+    arg->curr = new_user;
+    arg->list_of_users = client_list;
+    arg->ChatRoom_list = ChatRoom_list;
+    arg->mutex = &mutex;
+    arg->user_fileMutex = &user_fileMutex;
+    arg->group_fileMutex = &group_fileMutex;
+    arg->user_Map = user_Map;
+}
+
 int main() {
     atexit(cleanup);
     pthread_mutex_init(&mutex, NULL);
@@ -43,31 +56,24 @@ int main() {
 
     initUserMap(user_Map);
 
-    //create socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    //create our server's information
     struct sockaddr_in server;
     server.sin_family = AF_INET; //specify to use IPV4
     server.sin_port = htons(2520); // port is 2520
     server.sin_addr.s_addr = htonl(INADDR_ANY); // this is our ipv4 address
 
-    //bind socket to the port and server info
     bind(sock, (struct sockaddr*) &server, sizeof(server));
 
-    //waiting for a connection
     listen(sock, 25);
 
-    //we're gonna need our client list
     client_list = malloc(sizeof(user_list));
     init_user_list(client_list);
 
-    //we're gonna need our chatroom list
     ChatRoom_list = malloc(sizeof(ChatRoomList));
     init_ChatRoom_list(ChatRoom_list);
 
     while (1) {
-        //this is storing the clients information
         struct sockaddr_in client;
         socklen_t client_len = sizeof(client);
         int new_sock = accept(sock, (struct sockaddr*)&client, &client_len);
@@ -77,25 +83,15 @@ int main() {
             continue;
         }
 
-        //set up the new user (should get cleaned by the destructor)
         user *new_user = malloc(sizeof(user));
-        pthread_t id;
+
         new_user->client = client;
         new_user->next = NULL;
         new_user->sockid = new_sock;
-        new_user -> id = id;
 
         recv_exact_username(new_user->username, username_length, new_user->sockid);
 
-        //this is the thread argument, need the current user as well as the list
-        thread_arg *arg = malloc(sizeof(thread_arg));
-        arg->curr = new_user;
-        arg->list_of_users = client_list;
-        arg->ChatRoom_list = ChatRoom_list;
-        arg->mutex = &mutex;
-        arg->user_fileMutex = &user_fileMutex;
-        arg->group_fileMutex = &group_fileMutex;
-        arg->user_Map = user_Map;
+        thread_arg *arg = setupThreadArg(new_user);
         
         pthread_mutex_lock(&mutex);
 
